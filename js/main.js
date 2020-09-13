0beb960c06294f6c399106ed5eaf6488ed83e732
William Mannix <wmannix@gmail.com>
2020-09-12 19:22:53 -0600

    Con cambio de arrays a objetos y eliminar solo del array sin el select

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@ window.addEventListener('load', init, false);
 
 function init(e) {
 
-    var nombres = [];
-    var apellidos = [];
-    var notas = [];
-    var emails = [];
+    // var nombres = [];
+    // var apellidos = [];
+    // var notas = [];
+    // var emails = [];
 
-    var nombreInput = document.getElementById('nombreInput');
-    var apellidosInput = document.getElementById('apellidosInput');
-    var notaInput = document.getElementById('notaInput');
-    var emailInput = document.getElementById('emailInput');
+    // var nombreInput = document.getElementById('nombreInput');
+    // var apellidosInput = document.getElementById('apellidosInput');
+    // var notaInput = document.getElementById('notaInput');
+    // var emailInput = document.getElementById('emailInput');
+
+    var bdEstudiantes = [];
+
+    var estudiante = {nombreInput:document.getElementById('nombreInput'), apellidosInput:document.getElementById('apellidosInput'), notaInput:document.getElementById('notaInput'), emailInput:document.getElementById('emailInput')};
 
     var btnIngresar = document.getElementById('btnIngresar');
 
@@ -21,54 +25,10 @@ function init(e) {
 
     var btnBuscar = document.getElementById('btnBuscar');
 
+    var btnEliminar = document.getElementById('btnEliminar');
+
     agregarEstudiante('Mario', 'Lopez', '95', 'mlopez@mail.com');
     agregarEstudiante('Laura', 'Morales', '65', 'lmorales@mail.com');
-
-    //Debes usar el onkeyup que se dispara cada ves que se detecta que una tecla se levanto en el input.
-    //Aqui se hace para nombreInput pero puedes hacer uno para todos, osea sacar el codigo que se repite a una function.
-
-    // nombreInput.onkeyup = function (e) {
-    //     if (nombreInput.value === '') {
-    //         console.log(nombreInput.value);
-    //         nombreInput.className = 'error';
-    //     } else {
-    //         nombreInput.className = 'sinError';
-    //         console.log(nombreInput.value);
-    //     }
-    // }
-
-
-    // *** Opcion de onkeyup en una sola linea, *******
-    //     en cambia el estado en cada campo       
-
-    // nombreInput.onkeyup = function () { nombreInput.className = 'sinError' };
-    // apellidosInput.onkeyup = function () { apellidosInput.className = 'sinError' };
-    // notaInput.onkeyup = function () { notaInput.className = 'sinError' };
-    // emailInput.onkeyup = function () { emaiInput.className = 'sinError' };
-
-
-    var campos = ['nombreInput', 'apellidosInput', 'notaInput', 'emailInput'];
-    // var general;
-
-    // ****** Opcion con While ********
-
-    /*
-    //Notas: No me gusta ese while para esto pero alli esta.
-    //Investiga sobre className y classList
-    var i = 0;
-    while (i < campos.length) {
-        // general = document.getElementById(campos[i]);
-        document.getElementById(campos[i]).onkeyup = function (e) {
-            console.log(e);
-            if (e.target.value === '') {
-                e.target.classList.add('error');
-            } else {
-                e.target.classList.remove('error');
-            }
-        }
-        i++;
-    }
-    */
 
 
     //Nota: Prefiero esta solucion, ya tenias las referencias del input y ademas es mas facil de leer
@@ -85,34 +45,12 @@ function init(e) {
         }
     }
 
-
-    // ****** Opcion con For ********
-
-    // for ( var i = 0; i < campos.length; i++) {
-    //     general = document.getElementById(campos[i]);
-    //     console.log(general);
-    //     general.onkeyup = function (e) {
-    //         if (general === '') {
-    //             general.className = 'error';
-    //         } else {
-    //             general.className = 'sinError';
-    //         }
-    //     }
-    // }
-
     btnIngresar.onclick = function (e) {
 
         var isOk = true;
 
-        //Estoy aqui no esta bien ponerlo, estas haciendo lo mismo en cada click
-        // nombreInput.onchange = function() { nombreInput.className = 'sinError'};
-        // apellidosInput.onchange = function() { apellidosInput.className = 'sinError'};
-        // notaInput.onchange = function() { notaInput.className = 'sinError'};
-        // emailInput.onchange = function() { emailInput.className = 'sinError'};
-
         if (nombreInput.value === '') {
             isOk = false;
-            //nombreInput.style.borderColor = errorColor;
             nombreInput.className = 'error';
         }
 
@@ -140,10 +78,30 @@ function init(e) {
 
     }
 
+    function agregarEstudiante(nombre, apellidos, nota, email) {
+
+        bdEstudiantes.push({nombreInput:nombre, apellidosInput:apellidos, notaInput:nota, emailInput:email});
+
+        console.log(bdEstudiantes);
+
+        var index = bdEstudiantes.length - 1;
+        console.log(index);
+
+        var option = document.createElement('option');
+        option.value = index;
+        option.innerHTML = bdEstudiantes[index].nombreInput + ' ' + bdEstudiantes[index].apellidosInput;
+        estudianteSlt.appendChild(option);
+
+        console.log(option);
+    }
+
     btnBuscar.onclick = function (e) {
         var indexEstudiante = Number(estudianteSlt.value);
-        document.getElementById('informacion').innerHTML = 'Información:' + ' ' + nombres[indexEstudiante] + ' ' + apellidos[indexEstudiante] + ',' + ' ' + 'nota' + ' ' + notas[indexEstudiante] + ',';
-        if(notas[indexEstudiante] >= 70) {
+        console.log(indexEstudiante);
+
+        document.getElementById('informacion').innerHTML = 'Información:' + ' ' + bdEstudiantes[indexEstudiante].nombreInput + ' ' + bdEstudiantes[indexEstudiante].apellidosInput + ',' + ' ' + 'nota' + ' ' + bdEstudiantes[indexEstudiante].notaInput + ',';
+
+        if(bdEstudiantes[indexEstudiante].notaInput >= 70) {
             document.getElementById('aprobado').innerHTML = 'Abrobado';
             document.getElementById('aprobado').style.color = '#27ae60';
         } else {
@@ -153,21 +111,21 @@ function init(e) {
         console.log(indexEstudiante);
     }
 
-    function agregarEstudiante(nombre, apellido, nota, email) {
-        nombres.push(nombre);
-        apellidos.push(apellido);
-        notas.push(nota);
-        emails.push(email);
-        var index = nombres.length - 1;
-        console.log(index);
+    btnEliminar.onclick = function (e) {
+        var indexEstudiante = Number(estudianteSlt.value);
+        console.log(indexEstudiante);
 
-        var option = document.createElement('option');
-        option.value = index;
-        option.innerHTML = nombres[index] + ' ' + apellidos[index];
-        estudianteSlt.appendChild(option);
+        bdEstudiantes.splice(indexEstudiante,1);
+        console.log(bdEstudiantes);
 
-        console.log(option);
-    }
+        // var option  = document.createElement('option');
+        // option.value = indexEstudiante - 1;
+        // option.innerHTML = bdEstudiantes[index].nombreInput + ' ' + bdEstudiantes[index].apellidosInput;
+        // estudianteSlt.appendChild(option);
+
+
+        // estudianteSlt.removeChild(option);
+    };
 
     function clearInputs(e) {
         nombreInput.value = '';
